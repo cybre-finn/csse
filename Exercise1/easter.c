149b14612646293573691a52dc685eacc6cad5b0
Finn <ikarulus@zer.one>
2025-10-21 23:47:27 +0200

    added meaningful variable names for the knuth algo

--- Exercise1/easter.c
+++ Exercise1/easter.c
@@ -8,28 +8,41 @@ int calculate_Easter_date(int year) {
         return 0;
     }
 
-    int Y = year;
-    int G = (Y % 19) + 1;                 /* E1 */
-    int C = (Y / 100) + 1;                /* E2 */
-    int X = (3 * C / 4) - 12;
-    int Z = ((8 * C + 5) / 25) - 5;
-    int D = (5 * Y / 4) - X - 10;         /* E4 */
+   /* E1: golden year in the 19-year metoinic cycle */
+    int metonic_cycle = (year % 19) + 1;
 
-    int E = (11 * G + 20 + Z - X) % 30;   /* E5 */
-    if (E < 0) E += 30;
-    if ((E == 25 && G > 11) || E == 24) E++;
+    /* E2: Century */
+    int century = (year / 100) + 1;
 
-    int N = 44 - E;                       /* E6 */
-    if (N < 21) N += 30;
+    /* E3: Skipped leap years and moons orbit correction */
+    int skipped_leap_years = (3 * century / 4) - 12;
+    int moon_orbit_correction = ((8 * century + 5) / 25) - 5;
 
-    int r = (D + N) % 7;                  /* E7 */
-    if (r < 0) r += 7;
-    N = N + 7 - r;
+    /* E4: Adj for Sunday offset */
+    int sunday_offset = (5 * year / 4) - skipped_leap_years - 10;
 
-    if (N > 31)
-        return N - 31;    /* April: positive day */
+    /* E5: epact (age of moon on Jan 1) */
+    int epact = (11 * metonic_cycle + 20 + moon_orbit_correction - skipped_leap_years) % 30;
+    if (epact < 0) epact += 30;
+    if ((epact == 25 && metonic_cycle > 11) || epact == 24)
+        epact++;
+
+    /* E6: Calendar full moon (March N) */
+    int calendar_full_moon = 44 - epact;
+    if (calendar_full_moon < 21)
+        calendar_full_moon += 30;
+
+    /* E7: Sunday after full moon */
+    int sunday_after_full_moon = (sunday_offset + calendar_full_moon) % 7;
+    if (sunday_after_full_moon < 0)
+        sunday_after_full_moon += 7;
+    int easter_day_in_march = calendar_full_moon + 7 - sunday_after_full_moon;
+
+    /* E8: determine  month */
+    if (easter_day_in_march > 31)
+        return easter_day_in_march - 31;  /* April → positive day */
     else
-        return -N;        /* March: negative day */
+        return -easter_day_in_march;      /* March → negative day */
 }
 
 int main(int argc, char *argv[])
