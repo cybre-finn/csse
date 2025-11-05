4c5c9045229a74f90a04f306926c9c3e8fcc3353
Finn <ikarulus@zer.one>
2025-11-05 23:51:38 +0100

    "improve" pointer implementation

--- Exercise3/ptr_impl.c
+++ Exercise3/ptr_impl.c
@@ -1,15 +1,23 @@
 #include "impl.h"
 
 void update(int * current_gen, int * next_gen, size_t n_cells) {
+    /* I'm not using 3 pointers */
+    int *c_ptr;
+    int *n_ptr;
     int i;
-        for(i=1; i<n_cells-1; ++i) {
-            if((*(current_gen+(i-1)) == 1 && *(current_gen+(i+1)) == 0) ||
-               (*(current_gen+(i-1)) == 0 && *(current_gen+(i+1)) == 1)) {
-                *(next_gen+i) = 1;
+    c_ptr = current_gen+1;
+    n_ptr = next_gen+1;
 
-            }
-            else {
-                *(next_gen+i) = 0;
-            }
+    for(i=1; i<n_cells-1; ++i) {
+        if((*(c_ptr-1) == 1 && *(c_ptr+1) == 0) ||
+            (*(c_ptr-1) == 0 && *(c_ptr+1) == 1)) {
+            *n_ptr = 1;
+
+        }
+        else {
+            *n_ptr = 0;
+        }
+        c_ptr++;
+        n_ptr++;
     }
 }
