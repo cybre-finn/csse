0e14d62715dcf43c27d57fc63e9a5c6c1a6eb11c
Finn <ikarulus@zer.one>
2025-11-11 20:14:24 +0100

    some problem I found later

--- Exercise3/arr_impl.c
+++ Exercise3/arr_impl.c
@@ -3,8 +3,9 @@
 void update(int * current_gen, int * next_gen, size_t n_cells) {
     int i;
         for(i=1; i<n_cells-1; ++i) {
-            if((current_gen[i-1] == 1 && current_gen[i+1] == 0) ||
-               (current_gen[i-1] == 0 && current_gen[i+1] == 1)) {
+            if(((current_gen[i-1] == 1 && current_gen[i+1] == 0)
+               || (current_gen[i-1] == 0 && current_gen[i+1] == 1))
+               && current_gen[i] == 0) {
                 next_gen[i] = 1;
                 
             }
